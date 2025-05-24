96dbc86aba7c30dc583aae6aeadc5dcb896b572d
revanth <revanth.sn@gmail.com>
2025-05-24 10:15:23 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-04ccc30b04ef49701" 
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z022707230EX5QM3U8XUK"
 DOMAIN_NAME="chinni.fun"
 
