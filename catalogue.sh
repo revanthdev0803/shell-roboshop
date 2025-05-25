b73ca2df8522333342aba07aac7d3fcea388a041
revanth <revanth.sn@gmail.com>
2025-05-25 16:17:41 +0530

    first

--- catalogue.sh
+++ catalogue.sh
@@ -86,10 +86,10 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installed Mongodb"
 
 #To check wethere DB already exists or not 1 means exists lesser than 1 means not exists
-STATUS=$(mongosh --host mongodb.roboshop.space --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.chinni.fun --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.roboshop.space </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.chinni.fun </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loaded data"
 else
     echo -e "Catalogue DB already exists... $M SKIPPING $N"
