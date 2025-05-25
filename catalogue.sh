df4746b07200959620de90cf9f0a33748ba58f81
revanth <revanth.sn@gmail.com>
2025-05-25 16:12:34 +0530

    first

--- catalogue.sh
+++ catalogue.sh
@@ -1,87 +1,101 @@
 #!/bin/bash
 
-#this code is used to check either user was root or not
-USERID=$(id -u) #id -u commands extract only user information from log
-
-#these are the variables that use to store the colors
-
-R="\e[31m" #red
-G="\e[32m" #green
-Y="\e[33m" #yellow
-N="\e[0m" #no colour
-
-LOGS_FOLDER="/var/log/shellscripts-logs"    #we create this path for storing our logs
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)  # it removes the .sh in script name
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+M="\e[35m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)       #It will split the scriptName and gives only 10-logs which is field 1
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
-mkdir -p $LOGS_FOLDER #we are making directry and telling if it is already dont give error by using -p
-echo "script started at: $(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo -e "$M Script executing at : $N $(date)"  | tee -a $LOG_FILE
 
-#we are checking that if user was not equal to zero or not
-#root user id will be zero,if not we will give error
-
-if [ $USERID -ne 0 ]
+if [ $USERID -eq 0 ]   
 then
-    echo -e "$R Erorr....$N please run with root user" | tee -a $LOG_FILE 
-    exit 1 #give any number except zero for checking status
+    echo -e "$M Running with sudo user... $N" | tee -a $LOG_FILE
 else
-    echo "you are the root user" |&>>$LOG_FILE
+    echo -e "$R Error:: Run with sudo user to install packages $N" | tee -a $LOG_FILE
+    exit 1
 fi
 
- #we use this function if given one is installed or not ,to reduce the steps we use this
- #here we are sending two arguments to the function one is exit status as $1=$? $2=is the package name to install
+#function to validate package installed succesfully or not
 VALIDATE(){
 
     if [ $1 -eq 0 ]
     then
-        echo -e "$2 is $G succesfull $N" | tee -a $LOG_FILE 
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo "$2 is fail" | tee -a $LOG_FILE 
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
-dnf module disable nodejs -y
-VALIDATE $? "disabling default nodejs"
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabled existing nodejs version"
 
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabled required nodejs version"
 
-id roboshop
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installed nodejs"
+
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop 
-    VALIDATE $? "creating robosho system user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Created roboshop user"
 else
-    echo -e "system user roboshop already created....$Y skipping $N"
+    echo -e "Roboshop User already exists... $Y SKIPPING $N"
 fi
 
-mkdir -p /app
-VALIDATE $? "creating app directory"
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "Created app dir"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
-cd /app 
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloaded the catalogue service"
+
 rm -rf /app/*
-unzip /tmp/catalogue.zip
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Unzipped the catalogue service"
 
-npm install
+cd /app
+npm install &>>$LOG_FILE
+VALIDATE $? "Installed npm pkgm"
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "Catalogue service pasted in systemd"
 
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Loaded the service"
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue &>>$LOG_FILE
+VALIDATE $? "Catalogue service started"
 
-STATUS=$(mongosh --host mongodb.chinni.fun --quiet --eval 'db.getSiblingDB("catalogue").getCollectionNames().length')
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "Added mongo repo"
 
-if [ "$STATUS" -lt 0 ]
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installed Mongodb"
+
+#To check wethere DB already exists or not 1 means exists lesser than 1 means not exists
+STATUS=$(mongosh --host mongodb.roboshop.space --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.chinni.fun </app/db/master-data.js
-    VALIDATE $? "importing MongoDB schema"
+    mongosh --host mongodb.roboshop.space </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loaded data"
 else
-    echo -e "${Y}Data is already loaded, skipping import.${N}" | tee -a $LOG_FILE
+    echo -e "Catalogue DB already exists... $M SKIPPING $N"
 fi
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
