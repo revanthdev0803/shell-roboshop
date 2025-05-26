fd4e5021f97f6cd61f8fc5edf0385339c80bcb89
revanth <revanth.sn@gmail.com>
2025-05-26 15:01:26 +0530

    first

--- vijaya.sh
+++ vijaya.sh
@@ -13,7 +13,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t2.micro \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
    
     if [ $instance != "frontend" ]
     then
