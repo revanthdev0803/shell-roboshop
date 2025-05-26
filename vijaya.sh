86b9418243d3ec80f3eef76e961cabe083334428
revanth <revanth.sn@gmail.com>
2025-05-26 14:47:07 +0530

    first

--- vijaya.sh
+++ vijaya.sh
@@ -15,16 +15,17 @@ INSTANCE_ID=$(aws ec2 run-instances \
     --security-group-ids sg-063c02687e1103c7b \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]"--query "Instances[0].InstanceId" --output text)
    
-if [ $instance != "frontend" ]
-then
-    IP=$(aws ec2 describe-instances \
-    --instance-ids $instance \
-    --query "Reservations[0].Instances[0].PublicIpAddress" \
-    --output text)
-else
-	IP=$(aws ec2 describe-instances \
-    --instance-ids $instance \
-    --query "Reservations[0].Instances[0].PrivateIpAddress" \
-    --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $instance \
+        --query "Reservations[0].Instances[0].PublicIpAddress" \
+        --output text)
+    else
+	    IP=$(aws ec2 describe-instances \
+        --instance-ids $instance \
+        --query "Reservations[0].Instances[0].PrivateIpAddress" \
+        --output text)
 fi
+
 echo "$instace IP adress $IP" 
