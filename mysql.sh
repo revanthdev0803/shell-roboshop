0aa733ad29ef3cbaefc6a9c5b0fb87a24193ff86
revanth <revanth.sn@gmail.com>
2025-05-27 16:18:54 +0530

    first

---
+++ mysql.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+M="\e[35m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)       #It will split the scriptName and gives only 10-logs which is field 1
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo -e "$M Script executing at : $N $(date)"  | tee -a $LOG_FILE
+
+if [ $USERID -eq 0 ]   
+then
+    echo -e "$M Running with sudo user... $N" | tee -a $LOG_FILE
+else
+    echo -e "$R Error:: Run with sudo user to install packages $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+echo "please enter root password"
+read -s MYSQL_ROOT_PASSWORD
+
+
+#function to validate package installed succesfully or not
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installed nmy sql serverodejs"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "enabling mysql"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "starting mysql"
+
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+VALIDATE $? "setting password"
